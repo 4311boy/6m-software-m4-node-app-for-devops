a03d612bdc4bfd51f2fcf05bda2daf5620d9edc4
Luke <luke.tan@moovita.com>
2025-04-18 19:36:06 +0800

    updated deployment and also js script

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 const express = require('express')
 const app = express()
 
+// Use the PORT environment variable provided by Heroku
+const PORT = process.env.PORT || 3000;
+
 const print = require("./controller");
 
 app.get('/', print);
